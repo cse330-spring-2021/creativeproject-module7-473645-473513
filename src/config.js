79d0d1ca5e2a2c93722cf3869f321589b530c92f
miacollymoreabbas <mcollymoreabbas@wustl.edu>
2021-04-26 20:16:37 -0700

    displaying some things

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+// Replace with your app's client ID, redirect URI and desired scopes
+export const clientId = "5539e4309431435c8ae0b24a372730ff";
+export const redirectUri = "http://localhost:3000";
+export const scopes = [
+    "user-top-read",
+    "user-read-currently-playing",
+    "user-read-playback-state",
+];
